67ede4929f79c362d55515483900d455fdcfa13f
Kyoshiro Mibu <francois.caus@gmail.com>
2021-02-19 10:08:14 +0100

    docblock added

--- src/utils/randomizer.tsx
+++ src/utils/randomizer.tsx
@@ -1,3 +1,9 @@
+/**
+ * This functiun return a random number between min and max
+ * @param {number} min - the minimum value
+ * @param {number} max - the maximum value
+ * @return {number} - a random value calculated between min and max
+ */
 const getRandomizedNum = (min: number, max: number): number => {
   const minVal = Math.ceil(min);
   const maxVal = Math.floor(max);
