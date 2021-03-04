ee1cfcd1fc60c11bb0bea5062550171a311c3a49
oliv-bernier <olivbernier@gmail.com>
2021-03-04 15:42:05 +0100

    fixes on advices

--- src/components/Advices/index.tsx
+++ src/components/Advices/index.tsx
@@ -41,9 +41,9 @@ const Advices = ({
       <div className="advices-style">
         <p className="advice-style-message">
           Hello,
-          je suis un algorythme qui va vous conseiller sur la composition d'une équipe Pokémon.
+          je suis un algorithme qui va vous conseiller sur la composition d'une équipe Pokémon.
           <br />
-          Composez-la avec le pokédex en cliquant sur la pokéball !!
+          Composez-la avec le pokédex en cliquant sur la pokéball !
         </p>
       </div>
     </div>
