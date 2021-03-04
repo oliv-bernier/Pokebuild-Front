1026abfe6b3b66a730adccd07c10d134380262fa
Kyoshiro Mibu <francois.caus@gmail.com>
2021-03-04 09:21:34 +0100

    added the docBloc for the darkMode function

--- src/utils/darkMode.tsx
+++ src/utils/darkMode.tsx
@@ -1,3 +1,8 @@
+/**
+ * This function store the choice of the user regarding the dark mode
+ * by adding or removing the darkMode from the localstorage
+ * @param {boolean} isDark
+ */
 const darkMode = (isDark: boolean): void => {
   if (isDark) {
     localStorage.setItem('darkMode', 'enabled');
